e20044377025d65024dedfdb54432a121481e03a
Brittany Gahan <brittanygahan89@gmail.com>
2022-07-26 19:40:55 -0400

    update User schema to include cocktail information

--- server/models/User.js
+++ server/models/User.js
@@ -1,32 +1,29 @@
 const { Schema, model } = require('mongoose');
 const bcrypt = require('bcrypt');
 
+// import schema from Cocktail.js
+const cocktailSchema = require('./Cocktail');
+
 const userSchema = new Schema(
-  {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-      trim: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      match: [/.+@.+\..+/, 'Must match an email address!']
-    },
-    password: {
-      type: String,
-      required: true,
-      minlength: 5
-    },
-    cocktails: [
-      {
-        type: Schema.Types.ObjectId,
-        ref: 'Cocktail'
-      }
-    ]
-  },
+    {
+        username: {
+          type: String,
+          required: true,
+          unique: true,
+        },
+        email: {
+          type: String,
+          required: true,
+          unique: true,
+          match: [/.+@.+\..+/, 'Must use a valid email address'],
+        },
+        password: {
+          type: String,
+          required: true,
+        },
+        // set savedCocktails to be an array of data that adheres to the cocktailSchema
+        savedCocktails: [cocktailSchema],
+      },
   {
     toJSON: {
       virtuals: true
@@ -48,6 +45,11 @@ userSchema.pre('save', async function(next) {
 userSchema.methods.isCorrectPassword = async function(password) {
   return bcrypt.compare(password, this.password);
 };
+
+// when we query a user, we'll also get another field called `cocktailCount` with the number of saved cocktails we have
+userSchema.virtual('cocktailCount').get(function () {
+    return this.savedCocktails.length;
+  });
 
 const User = model('User', userSchema);
 
