0575826b8c545db0bf1ca38566401a20965b82a2
randronaco <randronaco1027@gmail.com>
2022-07-27 23:12:30 -0400

    updated home js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,3 +1,3 @@
-export const searchGoogleBooks = (query) => {
+export const searchCocktails = (query) => {
     return fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${query}`);
   };
