0976b9afce44543ec2859fb54ac91a4c77e2ca4d
Brittany Gahan <brittanygahan89@gmail.com>
2022-08-01 22:20:53 -0400

    add logic for saving drinks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,15 +2,21 @@ import React, { useState, useEffect } from 'react'
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import { searchCocktails } from '../utils/API';
 
+import { getSavedCocktailIds, saveCocktailIds } from '../utils/localStorage'
+
+import { useMutation } from '@apollo/client';
+import { SAVE_COCKTAIL } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 const Home = () => {
     const [searchedDrinks, setSearchedDrinks] = useState([]);
 
     const [searchInput, setSearchInput] = useState('');
 
-    // const [savedDrinkIds, setSavedDrinkIds] = useState(getSavedDrinkIds());
+    const [saveCocktailIds, setSaveCocktailIds] = useState(getSavedCocktailIds());
 
     // useEffect(() => {
-    //     return () => saveDrinkIds(savedDrinkIds);
+    //     return () => saveCocktailIds(saveCocktailIds);
     //   });
 
     const handleFormSubmit = async (event) => {
@@ -40,14 +46,14 @@ const Home = () => {
                 }]
                 console.log(drinks);
             }
-             
-                const drinkData = drinks.map((drink) => ({
-                    idDrink: drink.idDrink,
-                    strDrink: drink.strDrink,
-                    strInstructions: drink.strInstructions,
-                    strDrinkThumb: drink.strDrinkThumb,
-                })
-                );
+
+            const drinkData = drinks.map((drink) => ({
+                idDrink: drink.idDrink,
+                strDrink: drink.strDrink,
+                strInstructions: drink.strInstructions,
+                strDrinkThumb: drink.strDrinkThumb,
+            })
+            );
 
             setSearchedDrinks(drinkData);
             setSearchInput('');
@@ -55,59 +61,83 @@ const Home = () => {
             console.error(err);
         }
     }
+    
+    // save cocktail to database
+    const [addCocktail] = useMutation(SAVE_COCKTAIL);
+
+    const handleClick = async drinkId => {
+        const drinkInput = searchedDrinks.find(drink => drink.drinkId === drinkId)
+
+        try {
+          await addCocktail({
+            variables: { input: drinkInput},
+          })
+          
+          setSaveCocktailIds([...saveCocktailIds, drinkInput.drinkId]);
+        } catch (e) {
+          console.error(e);
+        }
+      };
+
     return (
         <>
-           <Jumbotron fluid className='text-light bg-dark'>
-                    <Container>
-                        <h1>Search for Drinks!</h1>
-                        <Form onSubmit={handleFormSubmit}>
-                            <Form.Group className="mb-2" controlId="searchForm">
-                                <Form.Control
-                                    name="searchInput"
-                                    value={searchInput}
-                                    onChange={(e) => setSearchInput(e.target.value)}
-                                    type="text"
-                                    placeholder="Enter drink name!" />
-                                <Form.Text className="text-muted">
-                                    Example: 'margarita' or 'strawberry daiquiri'
-                                </Form.Text>
-                            </Form.Group>
-
-                            <Button variant="primary" type="submit">
-                                Search
-                            </Button>
-                        </Form>
-                    </Container>
-                </Jumbotron>
-
+            <Jumbotron fluid className='text-light bg-dark'>
                 <Container>
-                    <h2>
-                        {searchedDrinks.length
-                            ? `Viewing ${searchedDrinks.length} results:`
-                            : 'Enter a cocktail!'}
-                    </h2>
-                    <CardColumns>
-                        {searchedDrinks.map((drink) => {
-                            return (
-                                <Card key={drink.idDrink} border='dark'>
-                                    {drink.strDrinkThumb ? (
-                                        <Card.Img src={drink.strDrinkThumb} alt={`The cover for ${drink.strDrink}`} variant='top' />
-                                    ) : null}
-                                    <Card.Body>
-                                        <Card.Title>{drink.strDrink}</Card.Title>
-                                        
-                                        <Card.Text>{drink.strInstructions}</Card.Text>
-                                        {Auth.loggedIn() && (
-                                            <Button>
-                                                Save Drink
-                                            </Button>
-                                        )}
-                                    </Card.Body>
-                                </Card>
-                            );
-                        })}
-                    </CardColumns>
+                    <h1>Search for Drinks!</h1>
+                    <Form onSubmit={handleFormSubmit}>
+                        <Form.Group className="mb-2" controlId="searchForm">
+                            <Form.Control
+                                name="searchInput"
+                                value={searchInput}
+                                onChange={(e) => setSearchInput(e.target.value)}
+                                type="text"
+                                placeholder="Enter drink name!" />
+                            <Form.Text className="text-muted">
+                                Example: 'margarita' or 'strawberry daiquiri'
+                            </Form.Text>
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Search
+                        </Button>
+                    </Form>
                 </Container>
+            </Jumbotron>
+
+            <Container>
+                <h2>
+                    {searchedDrinks.length
+                        ? `Viewing ${searchedDrinks.length} results:`
+                        : 'Enter a cocktail!'}
+                </h2>
+                <CardColumns>
+                    {searchedDrinks.map((drink) => {
+                        return (
+                            <Card key={drink.idDrink} border='dark'>
+                                {drink.strDrinkThumb ? (
+                                    <Card.Img src={drink.strDrinkThumb} alt={`The cover for ${drink.strDrink}`} variant='top' />
+                                ) : null}
+                                <Card.Body>
+                                    <Card.Title>{drink.strDrink}</Card.Title>
+
+                                    <Card.Text>{drink.strInstructions}</Card.Text>
+                                    {Auth.loggedIn() && (
+                                        <Button
+                                        disabled={saveCocktailIds?.some(saveCocktailId => saveCocktailId === drink.drinkId)}
+                                        className="btn-block btn-info"
+                                        onClick={() => handleClick(drink.drinkId)}
+                                    >
+                                        {saveCocktailIds?.some(saveCocktailId => saveCocktailId === drink.drinkId)
+                                            ? "This drink has been saved!"
+                                            : "Save this drink!"}
+                                    </Button>
+                                    )}
+                                </Card.Body>
+                            </Card>
+                        );
+                    })}
+                </CardColumns>
+            </Container>
         </>
     )
 }
--- client/src/pages/SavedDrinks.js
+++ client/src/pages/SavedDrinks.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import { useParams } from 'react-router-dom';
 
-const SaveDrinks = () => {
+import { useQuery } from '@apollo/client';
+import { GET_ME } from '../utils/queries';
+
+const SaveDrinks = (props) => {
+
+    const { username: userParam } = useParams();
+    const { loading, data } = useQuery(userParam ? GET_ME : {
+      variables: { username: userParam },
+    });
+
     return (
         <>
             <Jumbotron fluid className='text-light bg-dark'>
